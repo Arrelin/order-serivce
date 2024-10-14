a156a7c08026b1325ccf7f497f36f9c2cffa1f63
Arrelin <quickbluder@gmail.com>
2024-10-14 14:51:46 +0300

    changed flyway to liquibase cus its popular and added basic migrations for faster deploy

--- src/main/resources/db/migration/V1_init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE orders (
-                        id BIGSERIAL PRIMARY KEY,
-                        order_number VARCHAR(255) NOT NULL,
-                        sku_code VARCHAR(255) NOT NULL,
-                        price NUMERIC(19, 2) NOT NULL,
-                        quantity INTEGER NOT NULL
-);
