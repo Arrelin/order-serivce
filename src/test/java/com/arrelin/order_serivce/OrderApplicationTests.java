41ad25e3561de17566dabe9edabdd83bf7551683
Arrelin <quickbluder@gmail.com>
2024-10-16 14:44:59 +0300

    added more useless mockito tests

--- src/test/java/com/arrelin/order_serivce/OrderApplicationTests.java
+++ src/test/java/com/arrelin/order_serivce/OrderApplicationTests.java
@@ -1,31 +1,41 @@
 package com.arrelin.order_serivce;
 
+import com.arrelin.order.model.Order;
+import com.arrelin.order.repository.OrderRepository;
 import com.arrelin.order_serivce.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest(classes = com.arrelin.order.OrderApplication.class
 		, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWireMock(port = 0)
+@ActiveProfiles("test")
 class OrderApplicationTests {
 
 	@LocalServerPort
 	private Integer port;
 
+	@MockBean
+	private OrderRepository orderRepository;
+
 	@BeforeEach
 	void setup() {
 		RestAssured.baseURI = "http://localhost";
 		RestAssured.port = port;
+
+		Mockito.when(orderRepository.save(Mockito.any())).thenReturn(new Order());
 	}
 
 	@Test
--- src/test/java/com/arrelin/order_serivce/stubs/InventoryClientStub.java
+++ src/test/java/com/arrelin/order_serivce/stubs/InventoryClientStub.java
@@ -7,7 +7,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 public class InventoryClientStub {
 
     public static void stubInventoryCall(String skuCode, Integer quantity) {
-        stubFor(get(urlEqualTo("/inventory?skyCode=" + skuCode + "&quantity=" + quantity))
+        stubFor(get(urlEqualTo("/inventory?skuCode=" + skuCode + "&quantity=" + quantity))
                 .willReturn(aResponse()
                 .withStatus(200)
                 .withHeader("Content-Type", "application/json")
